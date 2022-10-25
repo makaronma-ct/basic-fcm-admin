76eef52053bfa8af7c6d261e578438022f460111
makaronma-ct <makaron.ma@cooltechsol.com>
2022-10-25 11:50:10 +0800

    chore: use esbuild+ts

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { credential } from 'firebase-admin';
+import { getApp, getApps, initializeApp } from 'firebase-admin/app';
+import { getMessaging } from 'firebase-admin/messaging';
+
+require("dotenv").config();
+
+const app =
+  getApps().length > 0
+    ? getApp()
+    : initializeApp({
+        credential: credential.cert(process.env.CREDENTIAL_PATH ?? ""),
+      });
+
+const messaging = getMessaging(app);
+messaging.send({
+  notification: {
+    title: "$FooCorp up 1.43% on the day",
+    body: "$FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.",
+  },
+  data: {
+    title: "p22",
+    body: "body123",
+    url: "example.com",
+    image: "image123",
+    sent_at: "Sent at123",
+  },
+  condition: "'all' in topics",
+});
